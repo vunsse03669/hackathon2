21d969c5bece77ea1399e6deb7abb206ab68778d
Quang Đại <quangdai.trinh@gmail.com>
2016-03-17 01:32:59 +0700

    Added files via upload
    Add new Helper about ENEMY Image

--- Tich chu/src/GameHelper/Helper.java
+++ Tich chu/src/GameHelper/Helper.java
@@ -6,7 +6,11 @@ package GameHelper;
 public class Helper {
     public static final int WIDTH = 1600;
     public static final int HEIGHT = 900;
-    public static final int FPS = 1000/60;
+    public static final int FPS = 1000 / 60;
     public static final int PLAYER_SPEED = 3;
     public static final int PLAYER_HP = 3;
+    public static final String ENEMY1 = "Resources/Image/Enemy1.png";
+    public static final String ENEMY2 = "Resources/Image/Enemy2.png";
+    public static final String ENEMY3 = "Resources/Image/Enemy3.png";
+    public static final String ENEMY4 = "Resources/Image/Enemy4.png";
 }
